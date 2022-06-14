43f0da6df52a02622cf9a41d78b53e640f165986
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-06-13 23:41:20 -0300

    Deploy teste heroku

---
+++ build/precache-manifest.aef1193f3a3a3222a8c48e81b0d63bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7eea4b87a120ea6412f1a6957c1d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3095ec2ddd92433dcfc",
+    "url": "/static/css/main.70f7588c.chunk.css"
+  },
+  {
+    "revision": "e9254bed71e867379469",
+    "url": "/static/js/2.ba215d25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba215d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3095ec2ddd92433dcfc",
+    "url": "/static/js/main.29ef83e0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
