4e87c48923356cd10711cb4f008905d8b94c1697
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-06-13 23:13:19 -0300

    Deploy exporess

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log('batata')
+const express = require('express')
+
+const { resolve } = require('path')
+
+const app = express()
+
+app.use('/', 
+
+    express.static(
+        
+        resolve(
+
+            __dirname,
+            './build'
+        )
+        
+     )
+    
+
+    )
+
+  
+  .listen(process.env.PORT || 3000, (err)=> {
+      if (err){ return console.log(err)}
+
+      console.log('Tudo funcionando')
+
+  })
+  
